ee7ad96d6d60d22b0933f4b357d6423ad2919094
jeffreywescott <jeffrey@learnersguild.org>
2017-03-28 03:30:10 -0700

    this doesn't seem necessary (and in fact breaks things)

--- server/util/formatSlackSAMLResponse.js
+++ server/util/formatSlackSAMLResponse.js
@@ -1,11 +1,15 @@
-// adds Slack-specific attributes to SAML response elements
-export default function formatSlackSAMLResponse(SAMLResponse, params = {}) {
-  const insertStr = ` NameQualifier="${params.NameQualifier}" SPNameQualifier="${params.SPNameQualifier}"`
-  const insertAfterStr = 'Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent"'
-  const insertIndex = SAMLResponse.indexOf(insertAfterStr) + insertAfterStr.length
-  return _insertAtIndex(SAMLResponse, insertStr, insertIndex)
-}
+// JW - 2017/03/28 -- this logic causes things to fail and doesn't seem necessary
 
-function _insertAtIndex(targetStr, insertStr, index) {
-  return `${targetStr.slice(0, index)}${insertStr}${targetStr.slice(index)}`
-}
+// adds Slack-specific attributes to SAML response elements
+// export default function formatSlackSAMLResponse(SAMLResponse, params = {}) {
+//   const insertStr = ` NameQualifier="${params.NameQualifier}" SPNameQualifier="${params.SPNameQualifier}"`
+//   const insertAfterStr = 'Format="urn:oasis:names:tc:SAML:2.0:nameid-format:persistent"'
+//   const insertIndex = SAMLResponse.indexOf(insertAfterStr) + insertAfterStr.length
+//   const response = _insertAtIndex(SAMLResponse, insertStr, insertIndex)
+//   console.log({response})
+//   return response
+// }
+//
+// function _insertAtIndex(targetStr, insertStr, index) {
+//   return `${targetStr.slice(0, index)}${insertStr}${targetStr.slice(index)}`
+// }
